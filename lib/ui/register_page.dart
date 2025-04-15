c0767980b12987f5fb64cff2bbc18cef60889ed3
Trương Ngọc Khuê <khuehn155@gmail.com>
2025-04-16 03:14:39 +0700

    feat(DangKyTaiKhoan): thêm tính năng đăng ký tài khoản

--- lib/ui/register_page.dart
+++ lib/ui/register_page.dart
@@ -1,14 +1,334 @@
+import 'package:app/api_config.dart';
+import 'package:app/ui/login_page.dart';
+import 'package:app/ui/verify_email_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../service/auth_service.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("register_page"),
+    final bool isSmallScreen = MediaQuery.of(context).size.width < 600;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Đăng ký tài khoản"),
       ),
+      body: Center(
+          child: SingleChildScrollView(
+        child: isSmallScreen
+            ? const Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  _FormContent(),
+                ],
+              )
+            : Container(
+                padding: const EdgeInsets.all(32.0),
+                constraints: const BoxConstraints(maxWidth: 800),
+                child: const Row(
+                  children: [
+                    Expanded(
+                      child: Center(child: _FormContent()),
+                    ),
+                  ],
+                ),
+              ),
+      )),
+    );
+  }
+}
+
+class _FormContent extends StatefulWidget {
+  const _FormContent();
+
+  @override
+  State<_FormContent> createState() => __FormContentState();
+}
+
+class __FormContentState extends State<_FormContent> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _cccdController = TextEditingController();
+
+  final storage = const FlutterSecureStorage();
+
+  final _authService = AuthService();
+
+  bool _isPasswordVisible = false;
+  bool _rememberMe = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  // Future<void> _checkLoginStatus() async {
+  //   bool isLoggedIn = await _authService.isLoggedIn();
+  //   if (isLoggedIn) {
+  //
+  //     if (_rememberMe) {
+  //       _saveUsername(_usernameController.text);
+  //     } else {
+  //       _deleteUsername();
+  //     }
+  //
+  //     Navigator.pushReplacement(
+  //       // ignore: use_build_context_synchronously
+  //       context,
+  //       MaterialPageRoute(
+  //           builder: (context) => MainPage(
+  //             username: _usernameController.text,
+  //           )),
+  //     );
+  //   }
+  // }
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints(maxWidth: 300),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: _cccdController,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Nhập căn cước công dân';
+                }
+
+                return null;
+              },
+              decoration: const InputDecoration(
+                labelText: 'Căn cước công dân',
+                hintText: 'Nhập căn cước công dân',
+                prefixIcon: Icon(Icons.badge_outlined),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            _gap(),
+            TextFormField(
+              controller: _emailController,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Nhập email';
+                }
+
+                return null;
+              },
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                hintText: 'Nhập email',
+                prefixIcon: Icon(Icons.email),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            _gap(),
+            TextFormField(
+              controller: _usernameController,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Nhập tên tài khoản';
+                }
+
+                return null;
+              },
+              decoration: const InputDecoration(
+                labelText: 'Tên tài khoản',
+                hintText: 'Nhập tên tài khoản',
+                prefixIcon: Icon(Icons.person_outlined),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            _gap(),
+            TextFormField(
+              controller: _passwordController,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Nhập mật khẩu';
+                }
+                if (value.length < 6) {
+                  return 'Mật khẩu phải có ít nhất 6 kí tự';
+                }
+                if (!RegExp(r'[^a-zA-Z0-9]').hasMatch(value)) {
+                  return 'Mật khẩu phải chứa ít nhất một ký tự không phải chữ cái';
+                }
+                if (!RegExp(r'[A-Z]').hasMatch(value)) {
+                  return 'Mật khẩu phải có ít nhất một chữ hoa';
+                }
+                if (!RegExp(r'[a-z]').hasMatch(value)) {
+                  return 'Mật khẩu phải có ít nhất một chữ thường';
+                }
+                return null;
+              },
+              obscureText: !_isPasswordVisible,
+              decoration: InputDecoration(
+                  labelText: 'Mật khẩu',
+                  hintText: 'Nhập mật khẩu',
+                  prefixIcon: const Icon(Icons.lock_outline_rounded),
+                  border: const OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    icon: Icon(_isPasswordVisible
+                        ? Icons.visibility_off
+                        : Icons.visibility),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      });
+                    },
+                  )),
+            ),
+            _gap(),
+            TextFormField(
+              controller: _confirmPasswordController,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Nhập lại mật khẩu';
+                }
+                if (_confirmPasswordController.text !=
+                    _passwordController.text) {
+                  return 'Mật khẩu cũ và mới phải giống nhau';
+                }
+                return null;
+              },
+              obscureText: !_isPasswordVisible,
+              decoration: InputDecoration(
+                labelText: 'Xác nhận mật khẩu',
+                hintText: 'Nhập lại mật khẩu',
+                prefixIcon: const Icon(Icons.lock_outline_rounded),
+                border: const OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  icon: Icon(_isPasswordVisible
+                      ? Icons.visibility_off
+                      : Icons.visibility),
+                  onPressed: () {
+                    setState(() {
+                      _isPasswordVisible = !_isPasswordVisible;
+                    });
+                  },
+                ),
+              ),
+            ),
+            _gap(),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4)),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    'Đăng ký',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF42A5F5),
+                    ),
+                  ),
+                ),
+                onPressed: () async {
+                  try {
+                    if (_formKey.currentState!.validate()) {
+                      String username = _usernameController.text;
+                      String password = _passwordController.text;
+                      String cccd = _cccdController.text;
+                      String email = _emailController.text;
+
+                      final response = await http.post(
+                          Uri.parse('${ApiConfig.baseUrl}/Account/Register'),
+                          headers: {
+                            'Content-Type': 'application/json',
+                          },
+                          body: jsonEncode({
+                            'username': username,
+                            'password': password,
+                            'email': email,
+                            'cccd': cccd
+                          }));
+
+                      if (response.statusCode == 200) {
+                        if (jsonDecode(response.body)['code'] == 200) {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('Thông báo'),
+                                content: const Text(
+                                    "Mã xác nhận đã được gửi về email vui lòng kiểm tra."),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  VerifyEmailPage(
+                                                      username: username)));
+                                    },
+                                    child: const Text('OK'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        }
+                      } else {
+                        _showAlertDialog('Có lỗi xảy ra vui lòng thử lại sau');
+                      }
+                    }
+                  } catch (e) {
+                    setState(() {
+                      _showAlertDialog(
+                          "Không thể kết nối đến máy chủ: Error $e");
+                    });
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _gap() => const SizedBox(height: 16);
+
+  void _showAlertDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Thông báo'),
+          content: Text(
+            message,
+            style: TextStyle(fontSize: 16),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
---
+++ lib/ui/verify_email_page.dart
@@ -0,0 +1,137 @@
+import 'package:app/ui/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:app/api_config.dart';
+
+class VerifyEmailPage extends StatefulWidget {
+  final String username;
+
+  const VerifyEmailPage({super.key, required this.username});
+
+  @override
+  _VerifyEmailPageState createState() => _VerifyEmailPageState();
+}
+
+class _VerifyEmailPageState extends State<VerifyEmailPage> {
+  final TextEditingController _verificationCodeController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  // Hàm gửi mã xác thực đến backend
+  Future<void> _verifyEmail() async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        final response = await http.post(
+          Uri.parse('${ApiConfig.baseUrl}/Account/VerifyEmail'),
+          headers: {'Content-Type': 'application/json'},
+          body: jsonEncode({
+            'userName': widget.username,
+            'verificationCode': _verificationCodeController.text,
+          }),
+        );
+
+        if (response.statusCode == 200) {
+          // Xác thực thành công, điều hướng về trang đăng nhập
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Thông báo'),
+                content: const Text('Xác nhận email thành công!'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => const LoginPage()),
+                      );
+                    },
+                    child: const Text('OK'),
+                  ),
+                ],
+              );
+            },
+          );
+        } else {
+          // Nếu có lỗi
+          _showAlertDialog('Mã xác nhận không đúng hoặc hết hạn.');
+        }
+      } catch (e) {
+        _showAlertDialog('Không thể kết nối đến máy chủ: $e');
+      }
+    }
+  }
+
+  // Hiển thị thông báo lỗi
+  void _showAlertDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Thông báo'),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Xác nhận email'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                controller: _verificationCodeController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Vui lòng nhập mã xác nhận';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Mã xác nhận',
+                  hintText: 'Nhập mã xác nhận',
+                  prefixIcon: Icon(Icons.confirmation_number_outlined),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: _verifyEmail,
+                  child: const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      'Xác nhận',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
